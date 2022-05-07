6931602d75e437c53004c91ec8c45e57746fa83c
VadimMaleev <maleeffset@yandex.by>
2022-05-07 17:36:33 +0300

    fix valid url

--- src/index.ts
+++ src/index.ts
@@ -85,7 +85,7 @@ app.delete('/bloggers/:id', (req: Request, res: Response) => {
 //Добавить Блоггера
 app.post('/bloggers', (req: Request, res: Response) => {
     if (req.body.name?.trim() === "" || req.body.name.length >= 15 || req.body.youtubeUrl?.trim() === "" || req.body.youtubeUrl.length >= 100 ||
-        !RegExp(/^https:\/\/([a-zA-Z0-9_-]+\.)+[a-zA-Z0-9_-]+(\/[a-zA-Z0-9_-]+)*\/?$/).exec(req.body.youtubeUrl)) {
+        RegExp(/^https:\/\/([a-zA-Z0-9_-]+\.)+[a-zA-Z0-9_-]+(\/[a-zA-Z0-9_-]+)*\/?$/).exec(req.body.youtubeUrl)) {
         return res.status(400).send({errorsMessages: [{message: 'string', field: "title"}], resultCode: 1})
     }
 
@@ -102,7 +102,7 @@ app.post('/bloggers', (req: Request, res: Response) => {
 app.put('/bloggers/:id', (req: Request, res: Response) => {
     if (typeof req.body.name !== "string" || req.body.name?.trim() === "" || req.body.name.length >= 15 ||
         typeof req.body.youtubeUrl !== "string" || req.body.youtubeUrl?.trim() === "" || req.body.youtubeUrl.length >= 100 ||
-        !RegExp(/^https:\/\/([a-zA-Z0-9_-]+\.)+[a-zA-Z0-9_-]+(\/[a-zA-Z0-9_-]+)*\/?$/).exec(req.body.youtubeUrl)
+        RegExp(/^https:\/\/([a-zA-Z0-9_-]+\.)+[a-zA-Z0-9_-]+(\/[a-zA-Z0-9_-]+)*\/?$/).exec(req.body.youtubeUrl)
     ) {
         return res.status(400).send({errorsMessages: [{message: 'string', field: "title"}], resultCode: 1})
     }
